9d4ce79a5a9dac4f3ac2dbba7605bdb9985ffb6b
waderaku <wadebosh@icloud.com>
2022-01-24 15:40:23 +0900

    initial commit

---
+++ pomodoro_timer/presentation/controller/native_user_controller.py
@@ -0,0 +1,27 @@
+from pomodoro_timer.presentation.controller.controller import Controller
+
+from django.http import HttpResponse, HttpRequest
+from pomodoro_timer.presentation.helper import get_id, get_body, create_response
+
+
+class NativeUserController(Controller):
+    def do_get(self, request: HttpRequest) -> HttpResponse:
+        id = get_id(request)
+        user = self._user_usecase.get_user_by_id(id)
+        response = {"id": user.id}
+        return create_response(response)
+
+    def do_post(self, request: HttpRequest) -> HttpResponse:
+        body = get_body(request)
+        code = body["authorizationCode"]
+
+        user = self._user_usecase.native_login(code)
+        response = {"id": user.id}
+        return create_response(response)
+
+    def do_put(self, request: HttpRequest) -> HttpResponse:
+        id = get_id(request)
+        body = get_body(request)
+        user = self._user_usecase.update_user(id, body)
+        response = {"id": user.id}
+        return create_response(response)
---
+++ pomodoro_timer/urls.py
@@ -0,0 +1,49 @@
+from django.urls import path
+
+from pomodoro_timer.domain.repository.iuser_repository import IUserRepository
+
+from pomodoro_timer.presentation.controller import (
+    LoginController,
+    CalendarController,
+    EventController,
+    TaskController,
+    TaskListController,
+    UserController,
+    UserConfigController,
+    NativeUserController,
+)
+
+from pomodoro_timer.domain.repository import (
+    ITokenRepository,
+    IGoogleRepository,
+    IUserRepository,
+)
+from pomodoro_timer.infrastructure.repository import (
+    TokenRepository,
+    GoogleRepository,
+    UserRepository,
+)
+
+from inject import Binder
+import inject
+
+
+def inject_config(binder: Binder):
+    binder.bind(IUserRepository, UserRepository())
+    binder.bind(ITokenRepository, TokenRepository())
+    binder.bind(IGoogleRepository, GoogleRepository(UserRepository()))
+
+
+inject.configure(inject_config)
+
+
+urlpatterns = [
+    path("task", TaskController()),
+    path("user", UserController()),
+    path("calendar", CalendarController()),
+    path("login", LoginController()),
+    path("native/login", NativeUserController()),
+    path("taskList", TaskListController()),
+    path("userConfig", UserConfigController()),
+    path("event", EventController()),
+]
